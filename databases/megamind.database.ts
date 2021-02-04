acacef6ca67e144b6cfa78bd2856b8f27a2c253c
Kasra_K2K <Kasra_K2K@yahoo.com>
2021-02-04 16:15:31 +0330

    Add comment to megamind database

--- databases/megamind.database.ts
+++ databases/megamind.database.ts
@@ -7,3 +7,9 @@ export const megamind = mongoose.createConnection(
 		useUnifiedTopology: true,
 	}
 );
+
+// Using MongoDB with Mongoose — Populate Virtuals
+
+// https://medium.com/dev-genius/using-mongodb-with-mongoose-populate-virtuals-91710f066de7
+
+// https://thewebdev.info/2020/09/26/using-mongodb-with-mongoose%e2%80%8a-%e2%80%8apopulate-virtuals/
